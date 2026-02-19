4157387ec78469cfe101195f981b7c4778da0068
Anish Mannem <81965254+anishmannem963@users.noreply.github.com>
2026-02-18 22:03:28 -0500

    Refactor main.go for config loading and app setup
    Refactor main.go to include configuration loading and Wails app initialization.

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"Sector/internal/config"
+	"embed"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+)
+
+var assets embed.FS
 
 func main() {
-	fmt.Println("Sector Backend - Sprint 1 Foundation")
+	config.LoadEnv()
+
+	// Create an instance of the app structure
+	app := NewApp()
+
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title: "Sector",
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		BackgroundColour: &options.RGBA{R: 255, G: 255, B: 255, A: 1},
+		OnStartup:        app.startup,
+		Bind: []interface{}{
+			app,
+		},
+	})
+
+	if err != nil {
+		println("Error:", err.Error())
+	}
 }
